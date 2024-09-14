52886c126aec20370af533b236657d761bb30139
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2024-09-15 02:25:55 +0600

    react router added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
+/* eslint-disable no-unused-vars */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/contact",
+    element: <div>This is contact page</div>
+  },
+  {
+    path:"/about",
+    element: <div>This is about page</div>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
